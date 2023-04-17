e8298596a5ec5026e09ebf6c30a27f8ff06077e3
naresh mahto <mahtonaresh059@gmail.com>
2023-04-17 23:58:48 +0530

    Updates

---
+++ precache-manifest.c5c9420bbffaa479bdf387fde8b8ec09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566061d2ac86890dcca03e95424b1cb8",
+    "url": "/Tooltip-react/index.html"
+  },
+  {
+    "revision": "1da06dbc53fe61d3f79c",
+    "url": "/Tooltip-react/static/css/main.3c30e4ca.chunk.css"
+  },
+  {
+    "revision": "39f2c1537d206e9168ea",
+    "url": "/Tooltip-react/static/js/2.0e2e7a76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tooltip-react/static/js/2.0e2e7a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1da06dbc53fe61d3f79c",
+    "url": "/Tooltip-react/static/js/main.f0d24ea3.chunk.js"
+  },
+  {
+    "revision": "cdc5e1c518b32712195a",
+    "url": "/Tooltip-react/static/js/runtime-main.6bf90da2.js"
+  }
+]);
